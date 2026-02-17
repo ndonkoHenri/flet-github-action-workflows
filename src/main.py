e05077cf413f3c8807faea1418263fb129f9c9da
ndonkoHenri <robotcoder4@protonmail.com>
2026-02-17 13:06:02 +0100

    initial commit

---
+++ src/main.py
@@ -0,0 +1,21 @@
+"""
+Flet Placeholder Example: https://docs.flet.dev/controls/placeholder/#basic-example
+"""
+
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.add(
+        ft.Placeholder(
+            expand=True,
+            color=ft.Colors.GREEN_ACCENT,
+            fallback_height=200,
+            fallback_width=300,
+            stroke_width=20,
+        )
+    )
+
+
+if __name__ == "__main__":
+    ft.run(main)
